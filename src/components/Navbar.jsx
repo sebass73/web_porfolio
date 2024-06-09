80600338fcfa61d4dfa1df37fba2792a572f084d
sebastian martinez <semartinez.6226@gmail.com>
2024-06-09 11:56:27 -0300

    ajustes en home y en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import "./Navbar.css";
 import { pageList } from "../constants/pageList";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import useWidthScreen from "../hooks/useWidthScreen";
+import argentinaFlag from "../../public/argentinaFlag.png";
+import UKFlag from "../../public/ukFlag.png";
 
 const Navbar = () => {
   const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(true);
@@ -34,79 +37,172 @@ const Navbar = () => {
     i18n.changeLanguage(lng);
   };
 
+  const widthScreen = useWidthScreen();
+
   return (
-    <nav
-      className={`navbar navbar-expand-lg ${
-        isScrolled ? "navbar-colored" : "navbar-transparent"
-      } navbar-light`}
-    >
-      <div className={`container-fluid ${isScrolled ? "d-flex" : "d-none"}`}>
-        <a className="navbar-brand" href="/">
-          Sebas.Dev
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={toggleNavbar}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className={`collapse navbar-collapse ${
-            isNavbarCollapsed ? "collapse" : "show"
-          }`}
-          id="navbarNav"
-        >
-          <ul className="navbar-nav mx-auto">
-            {pageList.map((page, i) => (
-              <li key={page + i} className="nav-item">
-                <a
-                  className="nav-link"
-                  aria-current="page"
-                  to={page.route}
-                  href={page.href}
-                  onClick={handleNavItemClick}
-                >
-                  {t(page.name)}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div className="switch-container">
+    <>
+      {widthScreen < 998 ? (
+        <>
+          <nav
+            className={`navbar navbar-expand-lg ${
+              isScrolled ? "navbar-colored" : "navbar-transparent"
+            } navbar-light`}
+          >
             <div
-              className="form-check-label me-2"
-              style={{ color: "wheat" }}
-              htmlFor=""
+              className={`container-fluid ${isScrolled ? "d-flex" : "d-none"}`}
             >
-              🇬🇧 English
+              <a className="navbar-brand" href="/">
+                Sebas.Dev
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={toggleNavbar}
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className={`collapse navbar-collapse ${
+                  isNavbarCollapsed ? "collapse" : "show"
+                }`}
+                id="navbarNav"
+              >
+                <ul className="navbar-nav mx-auto">
+                  {pageList.map((page, i) => (
+                    <li key={page + i} className="nav-item">
+                      <a
+                        className="nav-link"
+                        aria-current="page"
+                        to={page.route}
+                        href={page.href}
+                        onClick={handleNavItemClick}
+                      >
+                        {t(page.name)}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+                <div className="switch-container">
+                  <div
+                    className="form-check-label me-2"
+                    style={{ color: "wheat" }}
+                    htmlFor=""
+                  >
+                    🇬🇧 English
+                  </div>
+                  <div className="form-check form-switch">
+                    <input
+                      className="form-check-input me-2"
+                      type="checkbox"
+                      id="flexSwitchCheckDefault"
+                      onChange={() =>
+                        changeLanguage(i18n.language === "en" ? "es" : "en")
+                      }
+                      checked={i18n.language === "es"}
+                    />
+                  </div>
+                  <div
+                    className="form-check-label"
+                    style={{ color: "wheat" }}
+                    htmlFor=""
+                  >
+                    🇦🇷 Español
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="form-check form-switch">
-              <input
-                className="form-check-input me-2"
-                type="checkbox"
-                id="flexSwitchCheckDefault"
-                onChange={() =>
-                  changeLanguage(i18n.language === "en" ? "es" : "en")
-                }
-                checked={i18n.language === "es"}
-              />
-            </div>
+          </nav>
+        </>
+      ) : (
+        <>
+          <nav
+            className={`navbar navbar-expand-lg ${
+              isScrolled ? "navbar-colored" : "navbar-transparent"
+            } navbar-light`}
+          >
             <div
-              className="form-check-label"
-              style={{ color: "wheat" }}
-              htmlFor=""
+              className={"container-fluid d-flex"}
             >
-              🇦🇷 Español
+              <a
+                className={`navbar-brand ${isScrolled ? "d-flex" : "d-none"}`}
+                href="/"
+              >
+                Sebas.Dev
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={toggleNavbar}
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className={`collapse navbar-collapse ${
+                  isNavbarCollapsed ? "show" : "show"
+                }`}
+                id="navbarNav"
+              >
+                <ul className="navbar-nav mx-auto">
+                  {pageList.map((page, i) => (
+                    <li
+                      key={page + i}
+                      className={`nav-item ${isScrolled ? "d-flex" : "d-none"}`}
+                    >
+                      <a
+                        className="nav-link"
+                        aria-current="page"
+                        to={page.route}
+                        href={page.href}
+                        onClick={handleNavItemClick}
+                      >
+                        {t(page.name)}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+                <div className="switch-container">
+                  <div
+                    className="form-check-label me-2"
+                    style={{ color: "wheat" }}
+                    htmlFor=""
+                  >
+                    <img src={UKFlag} style={{width: "28px", height: "28px"}} alt="" />
+                  </div>
+                  <div className="form-check form-switch">
+                    <input
+                      className="form-check-input me-2"
+                      type="checkbox"
+                      id="flexSwitchCheckDefault"
+                      onChange={() =>
+                        changeLanguage(i18n.language === "en" ? "es" : "en")
+                      }
+                      checked={i18n.language === "es"}
+                    />
+                  </div>
+                  <div
+                    className="form-check-label"
+                    style={{ color: "wheat" }}
+                    htmlFor=""
+                  >
+                    <img src={argentinaFlag} style={{width: "28px", height: "28px"}} alt="" />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+          </nav>
+        </>
+      )}
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css";
 import avatar from "../assets/avatar.avif";
 import me from "../assets/me1.svg";
+import meDesktop from "../assets/me_desktop.svg";
 import useWidthScreen from "../hooks/useWidthScreen";
 
 import Draggable from "react-draggable";
@@ -48,21 +49,17 @@ const Home = () => {
         <div className="custom">
           <div className="welcome-text-desktop">
             <div className="home-text">
-              <h3 className="welcome">Hola, ¿que tal? 👋</h3>
-              <h1 className="title font-link-title">Yo soy Sebastian</h1>
-              <h2 className="role">FullStack Developer</h2>
+              <h3 className="welcome-desktop">{t('Home welcome')}</h3>
+              <h1 className="title font-link-title">{t('Home title desktop')}<span className="home-desktop-name">{t('Home title desktop name')}</span>!</h1>
+              <h2 className="role">{t('Home role')}</h2>
+              <h3 className="home-role-description-desktop">{t('Home role description desktop')}</h3>
             </div>
             <div className="home-buttons">
-{/*               <button
-                className="btn btn-primary me-2"
-                onClick={handleHablemosClick}
-              >
-                Hablemos!
-              </button> */}
-              <button className="btn btn-secondary">My CV</button>
+              <LetsTalkButton/>
+              <ResumeButton/>
             </div>
           </div>
-          <img src={avatar} className="rounded-img" alt={avatar} />
+          <img src={meDesktop} className="rounded-img" alt={avatar} />
         </div>
       )}
     </div>
