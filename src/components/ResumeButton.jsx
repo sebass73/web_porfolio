619feb771a36be34fc019938026a9a4472476830
sebastian martinez <semartinez.6226@gmail.com>
2024-06-12 20:44:57 -0300

    fix descarga de archivo

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 const ResumeButton = () => {
   const handleResumeClick = () => {
     const link = document.createElement("a");
-    link.href = "../../public/CV_SM_ES_2024.pdf";
+    link.href = "/CV_SM_ES_2024.pdf";
     link.download = "CV_SM_ES_2024.pdf";
     document.body.appendChild(link);
     link.click();
